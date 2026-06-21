bf51582f046c8fda1d3598408a1a920462fe6398
Toe Toe Naing <toetoenaing218@gmail.com>
2026-06-21 14:19:59 +0630

    project setup

---
+++ src/main/java/com/jobhunter/JobHunter/JobHunterApplication.java
@@ -0,0 +1,13 @@
+package com.jobhunter.JobHunter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobHunterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobHunterApplication.class, args);
+	}
+
+}
