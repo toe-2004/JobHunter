ed76ff0e1c2d8f3dc4cb2cf71286134df4e6e696
Toe Toe Naing <toetoenaing218@gmail.com>
2026-08-19 22:16:38 +0630

    update photo

--- src/main/java/com/jobhunter/JobHunter/controller/EmployerController.java
+++ src/main/java/com/jobhunter/JobHunter/controller/EmployerController.java
@@ -8,12 +8,15 @@ import com.jobhunter.JobHunter.model.Employer;
 import com.jobhunter.JobHunter.model.Job;
 import com.jobhunter.JobHunter.model.User;
 import com.jobhunter.JobHunter.repository.ApplicationRepository;
+import com.jobhunter.JobHunter.repository.EmployerRepository;
+import com.jobhunter.JobHunter.repository.UserRepository;
 import com.jobhunter.JobHunter.service.EmployerService;
 import com.jobhunter.JobHunter.service.UserService;
 
 import jakarta.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
@@ -23,9 +26,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/employer")
@@ -37,6 +48,10 @@ public class EmployerController {
     private ApplicationRepository applicationRepository;
     @Autowired
     private UserService userService;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private EmployerRepository employerRepository;
 
     @GetMapping("/register")
     public String page(Model model) {
@@ -119,5 +134,55 @@ public class EmployerController {
     public long getShortlistedCount(Employer employer) {
 
         return applicationRepository.countByJobEmployerAndStatus(employer,ApplicationStatus.SHORTLISTED);
+    }
+    @GetMapping("/profile-edit")
+    public String editProfile(
+            Model model,
+            Authentication auth) {
+
+        Employer employer = employerRepository.findByUserEmail(auth.getName())
+                        .orElseThrow(() -> new RuntimeException("Employer not found"));
+
+        model.addAttribute("employer", employer);
+
+        return "employer/edit-profile";
+    }
+    
+    @PostMapping("/profile-update")
+    public String updateProfile(
+            @RequestParam("name") String name,
+            @RequestParam(value = "profilePhoto", required = false)
+                    MultipartFile profilePhoto,
+            Authentication auth) throws IOException {
+
+        Employer employer = employerRepository.findByUserEmail(auth.getName())
+                        .orElseThrow(() ->
+                                new RuntimeException("Employer not found"));
+
+        User user = employer.getUser();
+
+ 
+        if (profilePhoto != null && !profilePhoto.isEmpty()) {
+
+            String uploadDir = "uploads/";
+
+            File directory = new File(uploadDir);
+
+            if (!directory.exists()) {
+                directory.mkdirs();
+            }
+
+            String fileName = UUID.randomUUID() + "_" + profilePhoto.getOriginalFilename();
+
+            Path filePath = Paths.get(uploadDir + fileName);
+
+            Files.write(filePath,profilePhoto.getBytes());
+
+            user.setProfilePhoto(fileName);
+        }
+
+        userRepository.save(user);
+
+        return "redirect:/employer/profile";
     }
 }
