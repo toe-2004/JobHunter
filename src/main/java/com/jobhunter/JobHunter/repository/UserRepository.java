b2fdb4844186698a6c3581a5e4adb795f8f340ff
hsuwailinn <hsuwailinnwailinn@gmail.com>
2026-08-27 19:14:32 +0630

    add seeding data and save changes

--- src/main/java/com/jobhunter/JobHunter/repository/UserRepository.java
+++ src/main/java/com/jobhunter/JobHunter/repository/UserRepository.java
@@ -1,20 +1,21 @@
 package com.jobhunter.JobHunter.repository;
+
 import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.jobhunter.JobHunter.model.User;
-
+import com.jobhunter.JobHunter.enumeration.Role;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long>{
+public interface UserRepository extends JpaRepository<User, Long> {
 
-	Optional<User> findByEmail(String email);
+    Optional<User> findByEmail(String email);
+
     boolean existsByEmail(String email);
-    boolean existsByName(String name);
-    
-   
 
-    
-   
+    boolean existsByName(String name);
+
+    long countByRole(Role role);
 }
